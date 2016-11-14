7553a287d0e63dacbd604a356b8e1cf8b410e4dc
agaar <agaar@users.noreply.github.com>
2016-11-14 17:37:19 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for(int i = 0; i < t; i++) {
+            String n = scanner.next();
+            for(int j = 0; j < n.length()/2; j++) {
+                System.out.print(n.charAt(j));
+            }
+            System.out.println();
+        }
+    }
+}
