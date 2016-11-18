62f57813bf15d849fb279f05772976bc8c92a675
agaar <agaar@users.noreply.github.com>
2016-11-18 14:55:26 +0100

    Add files via upload

---
+++ PTEST.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/** http://pl.spoj.com/problems/PTEST/ */
+
+import java.util.*;
+import java.lang.*;
+
+class PTEST {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner scanner = new Scanner(System.in);
+        int numA = scanner.nextInt();
+        int numB = scanner.nextInt();
+
+        System.out.println(numA + numB);
+    }
+}
